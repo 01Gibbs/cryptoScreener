a27506cf3c0fb90be05f320334b4036f007b30ab
Gibbs <danielgilbert49@hotmail.co.uk>
2023-06-27 11:37:22 +0100

    useContext + API Call to CoinGecko

--- src/context/CryptoContext.jsx
+++ src/context/CryptoContext.jsx
@@ -1,12 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, useLayoutEffect, useState } from "react";
 
+//Context Object
 export const CryptoContext = createContext({});
 
+//Context Provider Component
 export const CryptoProvider = ({ children }) => {
-  const [test, setTest] = useState("this is a test");
+  const [cryptoData, setCryptoData] = useState();
+
+  //API Call Function
+  const getCryptoData = async () => {
+    try {
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
+      )
+        .then((res) => res.json())
+        .then((json) => json);
+
+      console.log(data);
+      setCryptoData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useLayoutEffect(() => {
+    getCryptoData();
+
+    return () => {};
+  }, []);
 
   return (
-    <CryptoContext.Provider value={{ test, setTest }}>
+    <CryptoContext.Provider value={{ cryptoData }}>
       {children}
     </CryptoContext.Provider>
   );
